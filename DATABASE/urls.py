221154dd31e83e5dc4bc995f59009014b42068f9
kyounghoon <kkh2826@naver.com>
2022-04-10 19:41:54 +0900

    Make Schema list, Table list, RowCnt API

--- DATABASE/urls.py
+++ DATABASE/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TNDDBConnection
+from .views import TNDDBConnection, TNDDBInfo
 
 urlpatterns = [
-    path('connect/<str:dbms>', TNDDBConnection.as_view()),
+    path('connect/<str:dbms>/', TNDDBConnection.as_view()),
+    path('databaseinfo/', TNDDBInfo.as_view())
 ]
--- DATABASE/views.py
+++ DATABASE/views.py
@@ -1,4 +1,3 @@
-
 # Create your views here.
 import pymssql
 from rest_framework.views import APIView
@@ -37,6 +36,81 @@ class TNDDBConnection(APIView):
 
         result['ConnectionSuccess'] = connectionSuccess
         result['Message'] = message
+
+        print(result)
+
+        return JsonResponse(result)
+
+
+class TNDDBInfo(APIView):
+    def ConnectDB_MSSQL(self, request):
+
+        connection = None
+
+        server = request.data['server']
+        port = request.data['port']
+        username = request.data['username']
+        password = request.data['password']
+        database = request.data['database']
+        
+        try:
+            connection = pymssql.connect(server=server, port=port, user=username, password=password, database=database)
+        except:
+            connection = None
+
+        return connection
+
+    def get(self, request):
+
+        result = {
+            'SCHEMA_LIST': dict(),
+            'ConnectionSuccess': False,
+            'Message': ''
+        }
+        cursor = None
+        connectionObject = self.ConnectDB_MSSQL(request)
+
+        if connectionObject is None:
+            result['ConnectionSuccess'] = False
+            result['Message'] = 'DB 연결 실패'
+        else:
+            result['ConnectionSuccess'] = True
+            result['Message'] = 'DB 연결 성공'
+
+        query = '''
+SELECT TABLE_SCHEMA AS SCHEMA_LIST
+     , TABLE_NAME
+     , (SELECT I.rows AS ROW_CNT
+		  FROM sysindexes I INNER JOIN sysobjects O ON I.id = O.id
+		 WHERE I.indid < 2 
+		   AND O.xtype = 'U'
+		   AND O.name = a.TABLE_NAME) AS ROW_CNT
+  FROM INFORMATION_SCHEMA.TABLES A
+ WHERE 1=1
+   AND TABLE_TYPE = 'BASE TABLE'
+ORDER BY 1,2
+'''
+
+        try:
+            cursor = connectionObject.cursor(as_dict=True)
+            cursor.execute(query)
+        except:
+            result['QueryState'] = False
+            result['Message'] = 'DB 쿼리 실행 실패'
+
+        datas = cursor.fetchall()
+
+        for data in datas:
+            schema = data['SCHEMA_LIST']
+            table = data['TABLE_NAME']
+            rowCnt = data['ROW_CNT']
+
+            if schema not in result['SCHEMA_LIST'].keys():
+                result['SCHEMA_LIST'].setdefault(schema, {'TABLE_NAME': dict()})
+                result['SCHEMA_LIST'][schema]['TABLE_NAME'].setdefault(table, rowCnt)
+            else:
+                if table not in result['SCHEMA_LIST'][schema]['TABLE_NAME'].keys():
+                    result['SCHEMA_LIST'][schema]['TABLE_NAME'].setdefault(table, rowCnt)
 
         return JsonResponse(result)
 
